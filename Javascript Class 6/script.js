c6faf57c63de45250b1edb5a035d11d07ede41c7
Ibtihaj Khan <Ibtihaajkhaan@gmail.com>
2024-09-27 19:35:46 +0500

    Add files via upload

---
+++ Javascript Class 6/script.js
@@ -0,0 +1,61 @@
+
+
+
+var Class = Number(prompt("Enter your Roll number"));
+if (Class ) {
+  var RollNumber = prompt("Do you Present today,");
+  if (RollNumber == "Yes") {
+    alert("Present");
+  } else {
+    alert("Absent");
+  }
+} else if (Class) {
+  var cnic = prompt("Do you Absent today,");
+  if (cnic == "Yes") {
+    alert("Present");
+  } else {
+    alert("Absent");
+  }
+} else {
+  console.log("No");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
